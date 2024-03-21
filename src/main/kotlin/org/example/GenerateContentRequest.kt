aaf7ad1eb469535d4dba9250140ebeea9270eb5d
Takanori Ugai <ugai@fujitsu.com>
2024-03-21 14:41:31 +0900

    Rename ContentRequest to GenerateContentRequest.

--- src/main/kotlin/org/example/GenerateContentRequest.kt
+++ src/main/kotlin/org/example/GenerateContentRequest.kt
@@ -3,7 +3,7 @@ package org.example
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ContentRequest(
+data class GenerateContentRequest(
     val contents: List<Content>,
     val safetySettings: List<SafetySetting> = emptyList(),
     val generationConfig: GenerationConfig? = null,
